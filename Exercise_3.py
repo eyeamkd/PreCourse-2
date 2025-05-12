973248fae071062cbed589ffd4e133396739ddcb
Kunal Dubey <kunaldubey2297@gmail.com>
2025-05-12 11:09:01 +0530

    Improved Exercise-2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,30 +1,62 @@
 # Node class  
+"""
+Time Complexity: 
+init: O(1)
+push: O(1)
+
+PrintMiddle: O(n)
+
+
+Space Complexity:
+
+init: O(1)
+push: O(1) per call, total list space is O(n)
+printMiddle: O(1)
+
+"""
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
-        self.data = data
+    def __init__(self, data=None):
+        self.data = data 
         self.next = None
 
 class LinkedList: 
   
-    def __init__(self): 
+    def __init__(self):
         self.head = None
+        self.tail = None
   
     def push(self, new_data): 
         new_node = Node(new_data)
-        new_node.next = self.head
-        self.head = new_node
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            prev = self.tail 
+            prev.next = new_node 
+            self.tail = new_node
+
+        
+        
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
-        slow = self.head
-        fast = self.head
+    def printMiddle(self):
+        if self.head is None:
+            raise ValueError("List is empty")
+
+        slow = fast = self.head 
+        
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+            slow = slow.next 
+            fast = fast.next.next 
+        
         print(slow.data)
+        return 
+            
+            
+         
+        
 
 # Driver code 
 list1 = LinkedList() 
