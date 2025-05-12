e8316979b52d72d4778dfed2e3151f3d17036c8e
Kunal Dubey <kunaldubey2297@gmail.com>
2025-05-12 09:51:19 +0530

    Improved Exercise-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,26 +2,69 @@
 # Search. 
   
 # It returns location of x in given array arr  
-# if present, else returns -1 
-def binarySearch(arr, l, r, x):
-    while l <= r:
-        mid = l + (r - l) // 2
-        if arr[mid] == x:
-            return mid
-        elif arr[mid] < x:
-            l = mid + 1
-        else:
-            r = mid - 1
-    return -1
+# if present, else returns -1
+"""
+Time Complexity: O(log(n))
 
+Space Complexity: O(1)
+"""
+
+def binarySearch(arr, l, r, x):
+      if r<l:
+          raise ValueError("Improper Index passed")
+      if len(arr)==0:
+            raise ValueError("Array empty")
+      while l<=r:
+        mid = (l+r)//2 
+        element = arr[mid]
+        if element==x:
+              return mid 
+        if element < x:
+              l = mid+1
+        else:
+              r = mid-1 
+      
+      return -1
+
+def safeBinarySearch(arr, l, r, x):
+    if arr!= sorted(arr):
+      raise ValueError("Array must be sorted for Binary search")
+    return binarySearch(arr, l, r, x)
+
+
+
+      
 # Test array 
 arr = [2, 3, 4, 10, 40]
 x = 10
-  
+
 # Function call 
-result = binarySearch(arr, 0, len(arr)-1, x)
+result = safeBinarySearch(arr, 0, len(arr)-1, x)
   
 if result != -1: 
     print("Element is present at index %d" % result) 
 else: 
     print("Element is not present in array")
+
+arr = [1, 2, 4, 6, 8, 10]
+x = 20
+
+# Function call 
+result = safeBinarySearch(arr, 0, len(arr)-1, x)
+  
+if result != -1: 
+    print("Element is present at index %d" % result) 
+else: 
+    print("Element is not present in array")
+    
+arr = [2, 4, 1, 6, 8, 10]
+x = 6
+
+result = safeBinarySearch(arr, 0, len(arr)-1, x)
+  
+if result != -1: 
+    print("Element is present at index %d" % result) 
+else: 
+    print("Element is not present in array")
+  
+
