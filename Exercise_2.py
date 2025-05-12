13ea2da584e952d8164b6c5066d142473128f8e6
Kunal Dubey <kunaldubey2297@gmail.com>
2025-05-12 15:06:12 +0530

    Improved Exercise-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,22 +1,67 @@
 # Python program for implementation of Quicksort Sort 
+
+""" 
+Time Complexity:
+partition : O(n)
+
+quicksort : This function evenly divides the array into two parts and hence the recursive depth of the binary tree formed is log(n) 
+
+Total Complexity: O(nlogn)
+
+Worst Case: When the pivotal element is the lowest element or the highest element, then the quickSort function divides the array unevenly and the recursive depth might exceed until O(n),
+which makes the worst case time complexity to be O(n^2)
+
+
+Space: 
+
+sort_using_arrays: Is the implementation of the quickSort using an auxiallary array(s) hence it makes the space complexity to be O(n)
+
+parition: Is an in-place implementation so it makes the space complexity to be O(1)
+
+quickSort: O(logn) as it divides the arrays in the recursive call 
+"""
   
 # give you explanation for the approach
-def partition(arr, low, high):
+def sort_using_arrays(arr, low, high):
+    if low>=high or len(arr)==1:
+        return arr
     pivot = arr[high]
-    i = low - 1
-    for j in range(low, high):
-        if arr[j] <= pivot:
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
+    lessThanPivot = [ x for x in arr if x<pivot]
+    greaterThanPivot = [x for x in arr if x>pivot]
+    equalToPivot = [x for x in arr if x==pivot]
+            
+    return perform_sort(lessThanPivot, low, len(lessThanPivot)-1) + equalToPivot + perform_sort(greaterThanPivot, 0, len(greaterThanPivot)-1)
 
+def partition(arr, low, high):
+    if low == high:
+        return arr 
+    if low > high: 
+        return 
+
+    i = low-1 
+    j = low 
+    pivot = arr[high]
+    while j <= high - 1:
+        if arr[j] < pivot:
+            i+=1
+            temp = arr[j]
+            arr[j] = arr[i]
+            arr[i] = temp 
+        j+=1
+        
+    temp = arr[i+1]
+    arr[i+1] = arr[high] 
+    arr[high] = temp 
+    
+    return i+1
+            
 # Function to do Quick sort 
 def quickSort(arr, low, high):
-    if low < high:
+    if low<high:
         pi = partition(arr, low, high)
-        quickSort(arr, low, pi - 1)
-        quickSort(arr, pi + 1, high)
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+    
 
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
